5a26f63e1edb3b83e41ec32cd29a9f262b0822e1
PabloQuadros <pablo.aquadros@gmail.com>
2023-02-07 16:57:57 -0300

    Events and Common

---
+++ src/BuildingBlocks/EventBusMessages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBusMessages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent() 
+        {
+            Id = Guid.NewGuid();
+            CreationDate= DateTime.UtcNow;
+        }
+
+
+
+        public Guid Id { get;private set; }
+        public DateTime CreationDate { get; private set; }
+    }
+}
