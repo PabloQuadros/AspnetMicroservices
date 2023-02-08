30fce716b5840d2c405d255fd0d61fa9500c4c9a
PabloQuadros <pablo.aquadros@gmail.com>
2023-02-08 17:45:12 -0300

    .

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,41 +1,55 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using System.Net;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddStackExchangeRedisCache(options =>
+internal class Program
 {
-    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
-});
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+        });
 
 
-builder.Services.AddScoped<IBasketRepository,BasketRepository>();
-builder.Services.AddScoped<DiscountGrpcService>();
+        builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+        builder.Services.AddScoped<DiscountGrpcService>();
+        builder.Services.AddMassTransit(config =>
+        {
+            config.UsingRabbitMq((ctx, cfg) =>
+            {
+                cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+            });
+        });
 
-builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient> (o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
-builder.Services.AddScoped<DiscountGrpcService>();
-builder.Services.AddAutoMapper(typeof(Program));
+        builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
+        builder.Services.AddScoped<DiscountGrpcService>();
+        builder.Services.AddAutoMapper(typeof(Program));
 
-var app = builder.Build();
+        var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
