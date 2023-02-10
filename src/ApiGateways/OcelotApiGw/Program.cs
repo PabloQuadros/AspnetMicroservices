404dc800660eb14c6bff3b38fb40ffba5487ab11
PabloQuadros <pablo.aquadros@gmail.com>
2023-02-10 16:13:49 -0300

    Configure Ocelot Json

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,10 +1,14 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging")).AddConsole().AddDebug();
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+var app = builder.Build();
+
 app.MapGet("/", () => "Hello World!");
 await app.UseOcelot();
 app.Run();
