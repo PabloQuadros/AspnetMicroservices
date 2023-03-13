16fc8f1879b80c331643210acf1b1271f026dcf8
PabloQuadros <pablo.aquadros@gmail.com>
2023-03-13 17:03:07 -0300

    .

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,3 +1,5 @@
+using Common.Logging;
+using Serilog;
 using Shopping.Aggregator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,17 +7,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog(SeriLogger.Configure); 
+
+builder.Services.AddTransient<LoggingDelegatingHandler>();
 builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
-               c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]));
+               c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]))
+                    .AddHttpMessageHandler<LoggingDelegatingHandler>();
+
 
 builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
-    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]));
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]))
+                    .AddHttpMessageHandler<LoggingDelegatingHandler>();
+
 
 builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
-    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]));
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]))
+                    .AddHttpMessageHandler<LoggingDelegatingHandler>();
+
 
 
 var app = builder.Build();
@@ -26,6 +38,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseAuthorization();
 
