2e7d2b1e4e2a7b8f160eb38408423ce17c2ff2ec
PabloQuadros <pablo.aquadros@gmail.com>
2023-03-13 15:04:54 -0300

    .

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
 using System.Reflection;
+using Common.Logging;
 
 namespace AspnetRunBasics
 {
@@ -15,22 +16,7 @@ namespace AspnetRunBasics
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog((context,configuration)=>
-                {
-                    configuration.Enrich.FromLogContext()
-                    .Enrich.WithMachineName()
-                    .WriteTo.Console()
-                    .WriteTo.Elasticsearch(
-                        new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
-                        {
-                            IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-logs-{DateTime.UtcNow:yyyy-MM}",
-                            AutoRegisterTemplate = true,
-                            NumberOfShards = 2,
-                            NumberOfReplicas = 1
-                        })
-                    .Enrich.WithProperty("Enviroment", context.HostingEnvironment.EnvironmentName)
-                    .ReadFrom.Configuration(context.Configuration);
-                })
+                .UseSerilog(SeriLogger.Configure)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
