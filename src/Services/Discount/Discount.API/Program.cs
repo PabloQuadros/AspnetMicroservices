d6dfa4a1a73e83a74fee4454d5157004af14c582
PabloQuadros <pablo.aquadros@gmail.com>
2023-03-14 17:34:36 -0300

    Adding SeriLog for Discount.API

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,6 +1,7 @@
+using Common.Logging;
 using Discount.API.Extensions;
 using Discount.API.Repositories;
-
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Host.UseSerilog(SeriLogger.Configure);
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
