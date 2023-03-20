f1e4f931baaaab3d7c0fdcd6e8113d9a865cdc71
PabloQuadros <pablo.aquadros@gmail.com>
2023-03-20 16:41:25 -0300

    Serilog for OcelotApiGw

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,11 +1,13 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Cache.CacheManager;
+using Common.Logging;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging")).AddConsole().AddDebug();
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
-
+builder.Host.UseSerilog(SeriLogger.Configure);
 builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
 var app = builder.Build();
 
